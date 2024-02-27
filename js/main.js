d02dbabe7df25a2026185ef4c3b67976175ac7bb
Spirit Barnes <spiritbarnes19@gmail.com>
2024-02-27 13:17:59 -0800

    Created Neighbor Bombs

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-const rows = 10;
-const cols = 10;
-const totalBombs = 20;
+const rows = 10
+const cols = 10
+const totalBombs = 20
 
-let board = [];
-let bombs = [];
+let board = []
+let bombs = []
 
 function initializeBoard() {
   for (let r = 0; r < rows; r++) {
-    board[r] = [];
+    board[r] = []
     for (let c = 0; c < cols; c++) {
       board[r][c] = {
         isBomb: false,
@@ -21,20 +21,36 @@ function initializeBoard() {
 function presentBombs() {
   let bombsPlanted = 0;
   while (bombsPlanted < totalBombs) {
-    const row = Math.floor(Math.random() * rows);
-    const col = Math.floor(Math.random() * cols);
+    const row = Math.floor(Math.random() * rows)
+    const col = Math.floor(Math.random() * cols)
     
     if (!board[row][col].isBomb) {
-      board[row][col].isBomb = true;
-      bombs.push([row, col]);
-      bombsPlanted++;
+      board[row][col].isBomb = true
+      bombs.push([row, col])
+      bombsPlanted++
     }
   }
 }
 
+function createNeighborbombs() {
+  for (let i = 0; i < bombs.length; i++) {
+    let bomb = bombs[1]
+    for (let row = bomb.row -1; row <= bomb.row + 1; row++) {
+      for (let col = bomb.col -1; col <= bomb.col + 1; col++) {
+        if (row >= 0 && row < rows && col >= 0 && col < cols && !board[row][col].isBomb) {
+          board[row][col].neighborBombs++
+        }
+      }
+    }
+  }
+}
+
+
+
 function startGame () {
     initializeBoard()
     presentBombs()
+    createNeighborbombs()
 }
 
 startGame()
